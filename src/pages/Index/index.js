5ea9aa0a6752fc3dce78dcebeb1d27f3cffe3b5e
ymz <1823714291@qq.com>
2021-07-05 14:45:39 +0800

    rudex

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import store from "../../redux/store/index"
+
+
 export default class Index extends React.Component {
   constructor(props) {
     super(props)
@@ -7,6 +9,7 @@ export default class Index extends React.Component {
     this.state = store.getState()  // 把redux仓库store值赋值到this.state
     this.changeInputValue = this.changeInputValue.bind(this)
     this.storeChange = this.storeChange.bind(this)
+    this.clickBtn = this.clickBtn.bind(this)
     store.subscribe(this.storeChange)   // redux订阅模式组件改变 (Redux的状态)
   }
   changeInputValue(e) {
@@ -20,11 +23,35 @@ export default class Index extends React.Component {
   storeChange() {
     this.setState(store.getState)
   }
+  clickBtn() {
+    const action = {
+      type: 'ADD_ITEM',
+    }
+    store.dispatch(action)
+  }
+  deleteItem(index) {
+    const action = {
+      type: 'DELETE_ITEM',
+      value: index
+    }
+    store.dispatch(action)
+  }
   render() {
     return (
       <div>
         这是首页
         <input placeholder={this.state.inputValue} value={this.state.inputValue} onChange={this.changeInputValue}></input>
+        <button onClick={this.clickBtn}>增加</button>
+        <div style={{margin: '10px',border: '1px solid red'}}>
+          <ul>
+            {
+              this.state.list.map((v,index) => {
+                return <li key={index} onClick={this.deleteItem.bind(this, index)}>{v}</li>
+              })
+            }
+            
+          </ul>
+        </div>
       </div>
     )
   }
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,11 @@
 // 业务管理能力 reducers
 const defaultState = {
-  inputValue: 'text store'
+  inputValue: 'text store',
+  list: [
+    '早上八点开晨会，分配今天的任务',
+    '早九点和项目经理开需求沟通会',
+    '玩五点组织人员进行Review代码'
+  ]
 }
 export default (state = defaultState, action)=> {  // 暴露出去一个方法
   console.log(state, action)
@@ -9,6 +14,19 @@ export default (state = defaultState, action)=> {  // 暴露出去一个方法
     // 进行仓库数据改变
     let newState = JSON.parse(JSON.stringify(state))
     newState.inputValue = action.value
+    return newState
+  }
+  if (action.type === 'ADD_ITEM') {  // 新增
+    // 进行仓库数据改变
+    let newState = JSON.parse(JSON.stringify(state))
+    newState.list.push(newState.inputValue)
+    newState.inputValue = ''
+    return newState
+  }
+  if (action.type === 'DELETE_ITEM') {  // 移除
+    // 进行仓库数据改变
+    let newState = JSON.parse(JSON.stringify(state))
+    newState.list.splice(action.value, 1)
     return newState
   }
   return state
